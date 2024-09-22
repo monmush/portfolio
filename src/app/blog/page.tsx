dfdd23a49575054ffbdbea10f4533d709bc4845a
monmush <omiph001@gmail.com>
2024-09-22 11:35:33 +0700

    update blog post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -19,7 +19,7 @@ export default async function Blog() {
   return (
     <Container>
       <span className="text-4xl">📝</span>
-      <Heading className="font-black pb-4">I write about technology</Heading>
+      <Heading className="font-black pb-4">Sometimes I write</Heading>
       <Paragraph className="pb-10">
         Ever since <Highlight> I was a kid</Highlight>, I&apos;ve been
         fascinated by technology.
